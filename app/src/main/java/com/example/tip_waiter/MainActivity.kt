2c6cbace687f0716c0b6c48eeecbd92556f38176
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-02 00:43:19 +0300

    first edition

--- app/src/main/java/com/example/tip_waiter/MainActivity.kt
+++ app/src/main/java/com/example/tip_waiter/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tip_waiter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tip_waiter.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.roundToInt
@@ -16,10 +20,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.CalculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+
     }
 
     fun calculateTip() {
-        val inputNumberByUi = binding.costOfService.text.toString().toDouble()
+        val inputNumberByUi = binding.costOfServiceEditText.text.toString().toDouble()
 
         val amountOftipChosen = if (binding.AmazingButton.isChecked ) {inputNumberByUi * 1.2}
         else if (binding.GoodButton.isChecked) { inputNumberByUi * 1.18}
@@ -31,4 +37,15 @@ class MainActivity : AppCompatActivity() {
        val formattedAmount =  NumberFormat.getCurrencyInstance().format(amountOftipChosen)
         binding.tipResult.text = getString(R.string.tip_amount, formattedAmount)
     }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
